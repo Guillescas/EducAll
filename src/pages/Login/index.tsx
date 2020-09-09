678cffd42f80bf81d7e519be8ff58527048e2d2d
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-08 22:09:54 -0300

    Form structure initialized

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Form } from "@unform/web";
 
-import { Container, Background, Content } from "./styles";
+import { Container, Background, Content, IntroCard, LoginCard } from "./styles";
 
 import upOnBooks from "../../assets/teaching-white.svg";
 
@@ -8,11 +9,22 @@ const Login: React.FC = () => {
   return (
     <Container>
       <Background>
-        <img src={upOnBooks} alt="Professor" />
+        <IntroCard>
+          <h1>Sua plataforma de estudos</h1>
+          <p>
+            Pensamos nas suas dificuldades. Pensamos no seu aprendizado.
+            Pensamos em você!
+          </p>
+          <img src={upOnBooks} alt="Professor" />
+        </IntroCard>
       </Background>
 
       <Content>
-        <h1>Login</h1>
+        <LoginCard>
+          <Form onSubmit={() => {}}>
+            <h1>Login</h1>
+          </Form>
+        </LoginCard>
       </Content>
     </Container>
   );
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -9,6 +9,7 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
 
@@ -18,11 +19,39 @@ export const Content = styled.div`
 `;
 
 export const Background = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-
   img {
     width: 300px;
+  }
+`;
+
+export const IntroCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+  width: 750px;
+
+  h1 {
+    margin-top: -120px;
+    margin-bottom: 24px;
+  }
+
+  p {
+    max-width: 450px;
+    text-align: center;
+  }
+`;
+
+export const LoginCard = styled.div`
+  width: 380px;
+  height: 450px;
+  background-color: #3f3d56;
+  color: #f0f0f5;
+  border-radius: 8px;
+
+  h1 {
+    text-align: center;
   }
 `;
