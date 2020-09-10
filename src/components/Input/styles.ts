c7398fa5b863dff53e7ca9bfd2899a540103bb1d
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-10 14:03:07 -0300

    Basic input created

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: #fff;
+  border-radius: 8px;
+  padding: 16px;
+  width: 100%;
+
+  border: 2px solid #f0f0f5;
+  color: #3a3a3a;
+
+  display: flex;
+  align-items: center;
+
+  transition: color 0.2s, border 0.2s;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    flex: 1;
+    border: 0;
+    border-radius: 8px;
+    flex: 1;
+    height: 200px;
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
