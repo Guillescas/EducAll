8b53395ae6e18c730c4a15e4b424182bfe032108
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-20 19:53:04 -0300

    Add Left Menu component for application

---
+++ src/components/LeftMenu/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 325px;
+  background: #fff;
+
+  padding: 32px;
+  border-radius: 8px;
+  height: 500px;
+`;
